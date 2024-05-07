56a4acf6c603cc579c964754f60125ea2fc75948
mohyiddine <mohyiddine@gameaddik.com>
2024-05-07 08:57:09 -0400

    docs first push

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -9,7 +9,9 @@ class Command(BaseCommand):
 
     # Django command to wait for the database
     def handle(self, *args, **options):
-        # Entrypoint for command
+        '''
+        Entrypoint for command
+        '''
         self.stdout.write('Waiting for database...')
         db_up = False
         while db_up is False:
@@ -19,5 +21,5 @@ class Command(BaseCommand):
             except (Psycopg2Error, OperationalError):
                 self.stdout.write('Database unavailable, waiting 1 second...')
                 time.sleep(1)
-                
+
         self.stdout.write(self.style.SUCCESS('Database available!'))
